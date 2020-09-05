b9438e2066d5f7581eee296864e2f9eaf7c8368f
Paa-Gh <67100385+Paa-Gh@users.noreply.github.com>
2020-09-05 21:51:09 +0000

    deploy rails movie review app

---
+++ app/models/movie.rb
@@ -0,0 +1,7 @@
+class Movie < ApplicationRecord
+  belongs_to :user
+  has_many :reviews
+  validates :title, :image, presence:true
+  validates :title, uniqueness: true
+  has_one_attached :image
+end
